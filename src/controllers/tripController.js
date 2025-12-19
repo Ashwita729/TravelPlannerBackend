06eef589d558f26689e4c6fdc37c57fddadae246
Ashwita <ashwita.s2024aiml@sece.ac.in>
2025-12-19 13:57:17 +0530

    Fix duplicate Trip declaration in tripController

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -1,4 +1,3 @@
-HEAD
 const Trip = require('../models/Trip');
 
 const createTrip = async (req, res) => {
@@ -85,66 +84,4 @@ const deleteTrip = async (req, res) => {
   }
 };
 
-
-
-const Trip = require('../models/Trip');
-
-const createTrip = async (req, res) => {
-  try {
-    const { destination, startDate, endDate, description } = req.body;
-    const trip = await Trip.create({ 
-      user: req.user._id, 
-      destination, 
-      startDate, 
-      endDate, 
-      description 
-    });
-    res.status(201).json(trip);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const getTrips = async (req, res) => {
-  try {
-    const trips = await Trip.find({ user: req.user._id });
-    res.json(trips);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const updateTrip = async (req, res) => {
-  try {
-    const trip = await Trip.findById(req.params.id);
-    if (!trip) return res.status(404).json({ message: 'Trip not found' });
-    if (trip.user.toString() !== req.user._id.toString()) {
-      return res.status(401).json({ message: 'Not authorized' });
-    }
-
-    Object.assign(trip, req.body);
-    await trip.save();
-    res.json(trip);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const deleteTrip = async (req, res) => {
-  try {
-    const trip = await Trip.findById(req.params.id);
-    if (!trip) return res.status(404).json({ message: 'Trip not found' });
-    if (trip.user.toString() !== req.user._id.toString()) {
-      return res.status(401).json({ message: 'Not authorized' });
-    }
-
-    await Trip.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Trip removed' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-
->>>>>>> edc444351689a023e24478713c5f645222377be1
 module.exports = { createTrip, getTrips, updateTrip, deleteTrip };
