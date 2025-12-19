5bad1f21e9647c4a6a1b7d37038de4e2da43a8ca
Ashwita <ashwita.s2024aiml@sece.ac.in>
2025-12-19 14:00:03 +0530

    Fix duplicate jwt declaration in authMiddleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,5 @@
- HEAD
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
 
 const protect = async (req, res, next) => {
   let token;
@@ -36,35 +34,4 @@ const protect = async (req, res, next) => {
   }
 };
 
-
-
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
-
-const protect = async (req, res, next) => {
-  let token;
-
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      
-      if (!req.user) {
-        return res.status(401).json({ message: 'User not found' });
-      }
-      
-      next();
-    } catch (error) {
-      console.error('Auth middleware error:', error);
-      return res.status(401).json({ message: 'Not authorized, token failed' });
-    }
-  } else {
-    return res.status(401).json({ message: 'Not authorized, no token' });
-  }
-};
-
-
->>>>>>> edc444351689a023e24478713c5f645222377be1
 module.exports = { protect };
